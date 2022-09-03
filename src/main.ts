3f06904f19b23daf43f6d548bc7c235bc91d2849
cx2889 <523764720@qq.com>
2022-09-03 17:34:13 +0800

    输出没有成功翻译的内容

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ import CsvFS from "./CsvFS"
         oldVersionCsvMap.set(itemKey, itemValue)
     })
 
+    let cannotHandleList:string[] = []
     let devCsv = await new CsvFS().load("pre-data/localization/strings_dev-WWW.csv")
     await devCsv.eachData(async (itemValue, itemKey, index, dataList) =>
     {
@@ -19,10 +20,14 @@ import CsvFS from "./CsvFS"
         // {
         //     console.log(itemKey, index)
         // }
-        let finishWord = dictObj[itemKey] || oldVersionCsvMap.get(itemKey) || itemKey
-        
-        dataList[index] = itemKey + "," + finishWord
+        let finishWord = dictObj[itemKey] || oldVersionCsvMap.get(itemKey)
+        if(!finishWord)
+        {
+            cannotHandleList.push(itemKey + "," + itemValue)
+        }
+        dataList[index] = itemKey + "," + (finishWord || itemKey)
         // console.log(dataList[index]);
     })
     await devCsv.save("strings_zh-CN.csv")
+    await fs.writeFile("cannotHandleMap.txt", cannotHandleList.join("\r\n"), {encoding: "utf-8"})
 })()
